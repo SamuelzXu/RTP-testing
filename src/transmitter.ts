8a2f1b499d8911fa508d30b4564ac8bbbf107d85
Samuel Xu <samuelzxu@gmail.com>
2021-05-06 15:29:08 -0400

    Debugged and restructured spotty network functionality, and added command line arguments

--- src/transmitter.ts
+++ src/transmitter.ts
@@ -12,7 +12,7 @@ const MILLIS_PER_PACKET = 20;
 const MAX_DELAY_MILLIS = 60;
 
 const data = fs.readFileSync(INPUT_FILE);
-const numTotalPackets = Math.ceil(data.length / BYTES_PER_PACKET);
+let   numTotalPackets = Math.ceil(data.length / BYTES_PER_PACKET);
 const ssrc = Math.floor(Math.random() * Math.pow(2, 32));
 const initialTimestamp = Math.floor(Math.random() * 1000);
 const initialSequenceNumber = Math.floor(Math.random() * 1000);
@@ -21,6 +21,8 @@ const packetDuplicationProb = 0.03;
 const packetLossProb = 0.03;
 
 const client = dgram.createSocket("udp4");
+const spotty = process.argv[2] == "spotty"
+console.log(spotty);
 
 let packetsRead = 0;
 let packetsSent = 0;
@@ -31,42 +33,44 @@ const interval = setInterval(() => {
   const start = packetsRead * BYTES_PER_PACKET;
   const end = (packetsRead + 1) * BYTES_PER_PACKET;
 
-  // packet los and packet duplication are mutually exclusive
-  if (Math.random() <= packetLossProb) {
+  let loss = spotty ? false : Math.random() <= packetLossProb;
+  let dupl = spotty? false : Math.random() <= packetDuplicationProb;
+  
+  if (loss) {
     packetsRead++;
     packetsSent++;
-  } 
-  else if (Math.random() > packetDuplicationProb){
-    packetsRead++;
-  } 
-  else {
-    packetsSent--;
   }
+  else {
+    if (dupl) {
+      packetsSent--;
+    } else {
+      packetsRead++;
+    }
+    const packet = new RTPPacket();
+    packet.ssrc = ssrc;
+    packet.timestamp = initialTimestamp + end;
+    packet.sequenceNumber = initialSequenceNumber + packetsRead;
+    packet.payload = data.slice(start, end);
 
-  const packet = new RTPPacket();
-  packet.ssrc = ssrc;
-  packet.timestamp = initialTimestamp + end;
-  packet.sequenceNumber = initialSequenceNumber + packetsRead;
-  packet.payload = data.slice(start, end);
+    const timeout = Math.floor(Math.random() * MAX_DELAY_MILLIS);
 
-  const timeout = Math.floor(Math.random() * MAX_DELAY_MILLIS);
+    setTimeout(() => {
+      client.send(packet.data, RECEIVER_PORT, "localhost", (error) => {
+        if (error) {
+          console.error(error);
+          process.exit(1);
+        }
 
-  setTimeout(() => {
-    client.send(packet.data, RECEIVER_PORT, "localhost", (error) => {
-      if (error) {
-        console.error(error);
-        process.exit(1);
-      }
-
-      packetsSent++;
-
-      if (packetsSent === numTotalPackets) {
-        console.log(`Last packet sent`);
+        packetsSent++;
         console.log(`size ${packet.payload.length}`)
-        client.close();
-      }
-    });
-  }, timeout);
+        if (packetsSent === numTotalPackets) {
+          console.log(`Last packet sent`);
+          console.log(`size ${packet.payload.length}`)
+          client.close();
+        }
+      });
+    }, timeout);
+  }
 
   if (end >= data.length) {
     clearInterval(interval);
